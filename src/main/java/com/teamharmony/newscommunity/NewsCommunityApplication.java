20dd49790130b3f3056ef5ee4621d1fcb6615ec8
hyeoKing <cksgurwkd12@naver.com>
2022-07-27 18:58:41 +0900

    Fix: XSS 필터 적용 #213

--- src/main/java/com/teamharmony/newscommunity/NewsCommunityApplication.java
+++ src/main/java/com/teamharmony/newscommunity/NewsCommunityApplication.java
@@ -1,7 +1,9 @@
 package com.teamharmony.newscommunity;
 
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,5 +18,14 @@ public class NewsCommunityApplication {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+		FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new XssEscapeServletFilter());
+		registrationBean.setOrder(1);
+		registrationBean.addUrlPatterns("/write", "/restwrite");
+		return registrationBean;
 	}
 }
